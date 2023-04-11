efa595b035887006eca0576d0242c412ee4d7b77
Serovtsev Artem <1doncarleon@gmail.com>
2023-04-11 22:56:03 +0300

    add file

--- src/main/java/Posters.java
+++ src/main/java/Posters.java
@@ -7,5 +7,5 @@ import lombok.NoArgsConstructor;
 @Data
 public class Posters {
     private String productName;
+    }
 
-}
--- src/test/java/FilmsManagerTest.java
+++ src/test/java/FilmsManagerTest.java
@@ -12,6 +12,14 @@ class FilmsManagerTest {
     Posters film6 = new Posters("Номер один");
     Posters film7 = new Posters("Тестовый");
 
+    Posters film8 = new Posters("Зелёный слоник");
+
+    Posters film9 = new Posters("Брат");
+
+    Posters film10 = new Posters("Бумер");
+
+    Posters film11 = new Posters("Мальчишник Вегасе");
+
     @Test
     public void findLast() {
         FilmsManager manager = new FilmsManager();
@@ -121,5 +129,59 @@ class FilmsManagerTest {
         assertArrayEquals(actual, expected);
     }
 
+    @Test
+    public void findLastLowLim() {
+        FilmsManager manager = new FilmsManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
 
+        Posters[] actual = manager.findLast();
+        Posters[] expected = {film3, film2, film1};
+
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void findLastMoreLim() {
+
+       FilmsManager manager = new FilmsManager(10);
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+        manager.addFilm(film11);
+
+        Posters[] actual = manager.findLast();
+        Posters[] expected = {film11, film10, film9, film8, film7, film6, film5, film4, film3, film2};
+
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void findLastExactly10Lim() {
+
+     FilmsManager manager = new FilmsManager(10);
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+
+        Posters[] actual = manager.findLast();
+        Posters[] expected = {film10, film9, film8, film7, film6, film5, film4, film3, film2, film1};
+
+        assertArrayEquals(actual, expected);
+    }
 }
